dad0fbeda831ceea5a0f6a65827cc8e9c5691034
Abhilash Madhu <abhilashmadhu649@gmail.com>
2025-04-23 15:42:06 +0530

    first commit

---
+++ demoapp/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from . serializers import ProductSerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Product
+from django.http import JsonResponse
+
+
+
+@api_view(['GET'])
+def index(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+    
+@api_view(['POST', 'GET', 'PUT'])
+def readProduct(request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT','GET'])
+def updateProduct(request, pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(instance=product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def deleteProduct(request, pk):
+    product = Product.objects.get(id=pk)
+    product.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+
+
+
